91aedc3dd56c511caba42d83764ad46b81706d86
abdullahyaqoob <abdullahyaqoob38@gmail.com>
2023-09-29 17:20:09 +0500

    29 Sep

--- routes/SearchRoute.js
+++ routes/SearchRoute.js
@@ -1,6 +1,7 @@
 const route = require("express").Router();
 
 const InfoQuestQuestions = require("../models/InfoQuestQuestions");
+const BookmarkQuests = require("../models/BookmarkQuests");
 
 route.post("/easySearch", async (req, res) => {
   const searchTerm = req.query.term;
@@ -11,15 +12,27 @@ route.post("/easySearch", async (req, res) => {
         { Question: { $regex: searchTerm, $options: "i" } },
         { whichTypeQuestion: { $regex: searchTerm, $options: "i" } },
         {
-          "QuestAnswers.question": { $regex: searchTerm, $options: "i" }
-        }
-      ]
+          "QuestAnswers.question": { $regex: searchTerm, $options: "i" },
+        },
+      ],
     });
 
     const reversedResults = results.reverse();
 
     res.status(200).json(reversedResults);
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
 
+route.post("/searchBookmarks", async (req, res) => {
+  const searchTerm = req.query.term;
+  try {
+    const results = await BookmarkQuests.find({
+      $or: [{ Question: { $regex: searchTerm, $options: "i" } }],
+    });
+    const reversedResults = results.reverse();
+    res.status(200).json(reversedResults);
   } catch (err) {
     res.status(500).send("Internal Server Error");
   }
