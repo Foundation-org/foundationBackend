db0941d7a67edec939b7ee5aebee874c51601a9c
abdullahyaqoob <abdullahyaqoob38@gmail.com>
2023-09-30 18:39:48 +0500

    added check statements, and ai edit

--- constants/index.js
+++ constants/index.js
@@ -11,10 +11,14 @@ module.exports = {
     "do not have the ability",
     "unable to provide assistance",
     "not able to provide assistance",
+    "unable to assist",
+    "not able to assist"
   ],
   SYSTEM_MESSAGES: [
     "Correct provided statements to standard English. Shortest responses only.",
     "Correct provided words to standard English. Shortest responses only.",
-    "Tell me if the reply is relevant. Shortest answer only.",
+    // "Tell me if the reply is relevant. Shortest answer only.",
+    'I will give you a statement and a reply. Tell me if the reply is relevant. Shortest answer only.', // new
+    'Reject the statement if it is a fragment or non-sensical. Shortest answer only.' // new
   ],
 };
