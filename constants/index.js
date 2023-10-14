2653fb9bb2b36f33e94c19e7c623b05534b5399d
Muhammad Zubair <m.zobaire@gmail.com>
2023-10-14 15:10:27 +0500

    change the system message

--- constants/index.js
+++ constants/index.js
@@ -35,7 +35,7 @@ module.exports = {
     "Correct provided statement to standard English. Shortest responses only.",
     "Correct provided text to standard English. Shortest responses only.",
     // "Tell me if the reply is relevant. Shortest answer only.",
-    'I will give you a statement and a reply. Tell me if the reply is relevant. Shortest answer only.', // new
+    'I will give you a statement and a reply. Tell me if the reply can be relevant. Shortest answer only.', // new
     'Reject the statement if it is a fragment or non-sensical. Shortest answer only.' // new
   ],
 };
