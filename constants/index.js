51343635411e27a9e41470b652224ed4d26d794c
Muhammad Zubair <m.zobaire@gmail.com>
2023-11-03 17:57:58 +0500

    update the system message array

--- constants/index.js
+++ constants/index.js
@@ -14,7 +14,7 @@ module.exports = {
   SYSTEM_MESSAGES: [
     "Correct provided statement to standard English. Shortest responses only.",
     "Correct provided text to standard English. Shortest responses only.",
-    'I will give you a statement and a reply. Tell me if the reply can be relevant. Shortest answer only.', // new
-    'Reject the statement if it is a fragment or non-sensical. Shortest answer only.' // new
+    // 'I will give you a statement and a reply. Tell me if the reply can be relevant. Shortest answer only.', // new
+    // 'Reject the statement if it is a fragment or non-sensical. Shortest answer only.' // new
   ],
 };
