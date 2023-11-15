2aff971855bba998512e81024c7c1ca6f6ee6d70
Muhammad Zubair <m.zobaire@gmail.com>
2023-11-15 19:43:49 +0500

    AWS SES integration

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,7 +1,8 @@
 const User = require("../models/UserModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+const AWS = require("aws-sdk")
 const crypto = require("crypto");
 const { createToken } = require("../service/auth");
 const { createLedger } = require("../utils/createLedger");
@@ -177,61 +178,109 @@ const signedUuid = async (req, res) => {
     }
   }
 const sendVerifyEmail = async (req, res) => {
-    try {
-      const user = await User.findOne({ email: req.body.email });
-      // console.log("user", user);
-      !user && res.status(404).json("User not Found");
-  
-      const verificationTokenFull = jwt.sign(
-        { ID: user._id },
-        process.env.USER_VERIFICATION_TOKEN_SECRET,
-        { expiresIn: "1d" }
-      );
-  
-      const verificationToken = verificationTokenFull.substr(
-        verificationTokenFull.length - 6
-      );
-  
-      // const verificationToken = user.generateVerificationToken();
-      console.log("verificationToken", verificationToken);
-  
-      // Step 3 - Email the user a unique verification link
-      const url = `https://on.foundation/VerifyCode?${verificationTokenFull}`;
-      // console.log("url", url);
-  
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD,
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    // console.log("user", user);
+    !user && res.status(404).json("User not Found");
+
+    const verificationTokenFull = jwt.sign(
+      { ID: user._id },
+      process.env.USER_VERIFICATION_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    const verificationToken = verificationTokenFull.substr(
+      verificationTokenFull.length - 6
+    );
+
+    // const verificationToken = user.generateVerificationToken();
+    console.log("verificationToken", verificationToken);
+
+    // Step 3 - Email the user a unique verification link
+    const url = `https://on.foundation/VerifyCode?${verificationTokenFull}`;
+    // console.log("url", url);
+
+    // NODEMAILER 
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.EMAIL_USERNAME,
+    //     pass: process.env.EMAIL_PASSWORD,
+    //   },
+    // });
+
+    // var mailOptions = {
+    //   from: process.env.EMAIL_USERNAME,
+    //   to: req.body.email,
+    //   //   to: "abdullahyaqoob380@gmail.com",
+    //   subject: "Verify Account",
+    //   html: `Click <a href = '${url}'>here</a> to confirm your email <br /> <br /> <br />
+    //       And confirm this code <b>${verificationToken}</b> from the App`,
+    //   //   html: `Please Copy the code and Paste in App <b>${verificationToken}</b>`,
+    // };
+
+    // await transporter.sendMail(mailOptions, function (error, info) {
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log("email sent: " + info.response);
+    //   }
+    // });
+    const SES_CONFIG = {
+      region: process.env.AWS_SES_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    };
+    // Create SES service object
+    console.log("before sesClient", SES_CONFIG);
+
+    const sesClient = new AWS.SES(SES_CONFIG);
+
+    let params = {
+      Source: process.env.AWS_SES_SENDER,
+      Destination: {
+        ToAddresses: [
+          req.body.email
+        ]
+      },
+      ReplyToAddresses: [],
+      Message: {
+        Body: {
+          Html: {
+            Charset: 'UTF-8',
+            Data: `Click <a href = '${url}'>here</a> to confirm your email <br /> <br /> <br />
+                   And confirm this code <b>${verificationToken}</b> from the App`,
+          },
+          Text: {
+            Charset: 'UTF-8',
+            Data: 'Verify Accountt'
+          }
         },
-      });
-  
-      var mailOptions = {
-        from: process.env.EMAIL_USERNAME,
-        to: req.body.email,
-        //   to: "abdullahyaqoob380@gmail.com",
-        subject: "Verify Account",
-        html: `Click <a href = '${url}'>here</a> to confirm your email <br /> <br /> <br />
-          And confirm this code <b>${verificationToken}</b> from the App`,
-        //   html: `Please Copy the code and Paste in App <b>${verificationToken}</b>`,
-      };
-  
-      await transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("email sent: " + info.response);
+
+        Subject: {
+          Charset: 'UTF-8',
+          Data: 'Verify Account',
         }
-      });
-  
-      return res.status(201).send({
-        message: `Sent a verification email to ${email}`,
-      });
-    } catch (err) {
-      res.status(500).send(err);
+      },
     }
+
+    try {
+      const res = await sesClient.sendEmail(params).promise()
+      console.log("Email has been sent!", res);
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    return res.status(201).send({
+      message: `Sent a verification email to ${email}`,
+    });
+  } catch (err) {
+    res.status(500).send(err);
   }
+}
 const verify = async (req, res) => {
     const verificationCode = req.body.verificationCode;
     const token = req._parsedUrl.query;
