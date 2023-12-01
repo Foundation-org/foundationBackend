813baf0022ea8f617dc532cb3bf0c15a2bca6bae
wamiq <wamiqakram@gmail.com>
2023-12-01 10:42:24 +0500

    fixed Addmultiple

--- controller/InfoQuestQuestionController.js
+++ controller/InfoQuestQuestionController.js
@@ -13,7 +13,7 @@ const createInfoQuestQuest = async (req, res) => {
         whichTypeQuestion: req.body.whichTypeQuestion,
         usersAddTheirAns: req.body.usersAddTheirAns || false,
         usersChangeTheirAns: req.body.usersChangeTheirAns,
-        userCanSelectMutiple: req.body.userCanSelectMutiple,
+        userCanSelectMultiple: req.body.userCanSelectMultiple,
         QuestAnswers:
           req.body.QuestAnswers === undefined ? [] : req.body.QuestAnswers,
         QuestAnswersSelected:
