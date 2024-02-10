55d841d7767a2b5075630c698105b0416ea993d1
Muhammad Zubair <93026937+izubaire@users.noreply.github.com>
2024-02-11 02:52:30 +0500

    set size limit of question and options

--- middleware/aiValidationModerator.js
+++ middleware/aiValidationModerator.js
@@ -3,7 +3,28 @@ const { OPEN_AI_URL, OPEN_AI_KEY } = require("../config/env");
 const { SYSTEM_MESSAGES } = require("../constants");
 
 module.exports = async function (req, res, next) {
+  const callType = req.params.callType;
   const userMessage = req.query.userMessage;
+  // To check the question and statement not exceeded 500
+  if (callType == 1) {
+    if (userMessage.length >= 500)
+      return res
+        .status(400)
+        .json({
+          message: "Question / Statement should not exceeded 500 characters",
+          status: "ERROR",
+        });
+  }
+  // To check the question and statement not exceeded 500
+  if (callType == 2) {
+    if (userMessage.length >= 350)
+      return res
+        .status(400)
+        .json({
+          message: "Option should not exceeded 350 characters",
+          status: "ERROR",
+        });
+  }
   const response = await axios.post(
     OPEN_AI_URL,
     {
@@ -32,7 +53,7 @@ module.exports = async function (req, res, next) {
   //   To check the give value is only number
   if (typeof convertedNumber === "number" && !isNaN(convertedNumber)) {
     if (convertedNumber >= 0 && convertedNumber <= 100) {
-        // To check if the given number is greater than 75
+      // To check if the given number is greater than 75
       if (convertedNumber >= 75) {
         return res.json({ message: userMessage, status: "VIOLATION" });
       }
