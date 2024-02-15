a69bc10857199c0c79fdaa64c93d923903d4b8a6
Wamiq <wamiqakram@gmail.com>
2024-02-15 16:30:21 +0500

    ledger error removed

--- utils/createLedger.js
+++ utils/createLedger.js
@@ -1,19 +1,12 @@
 const Ledgers = require("../models/Ledgers");
 
-
 module.exports.createLedger = async (obj) => {
-    try {
-        const ledger = await new Ledgers({...obj});
-        const savedLedger = await ledger.save();
-        if(!savedLedger) throw new Error("Ledger Not Created Successfully!");
-        return savedLedger;
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: `An error occurred while create Ledger: ${error.message}` });
-      }
+  try {
+    const ledger = await new Ledgers({ ...obj });
+    const savedLedger = await ledger.save();
+    if (!savedLedger) throw new Error("Ledger Not Created Successfully!");
+    return savedLedger;
+  } catch (error) {
+    console.error(error);
+  }
 };
-
-
-// module.exports = {
-//     create,
-// }
