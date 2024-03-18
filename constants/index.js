b534deb681e16c6f08af3516bcbd407bc3d4efa6
Muhammad Zubair <93026937+izubaire@users.noreply.github.com>
2024-03-19 01:55:37 +0500

    add new category

--- constants/index.js
+++ constants/index.js
@@ -19,7 +19,7 @@ module.exports = {
     // OPTION
     "Correct text to standard English without contractions. Never correct brand names or informal words like Coke. Shortest responses only",
     // CATEGORY
-    "Give the closest category using only these: Architecture, Arts, Business, Culture, Economics, Education, Environment, Ethics, Geography, Health, History, Law, Pets, Music, Philosophy, Personal, Politics, Preferences, Psychology, Religion, Science, Society, Sports, Technology. Return one category only. Shortest responses only",
+    "Give the closest category using only these: Architecture, Arts, Business, Culture, Economics, Education, Entertainment, Environment, Ethics, Food, Geography, Health, History, Hobbies, Home, Law, Media, Music, Pets, Philosophy, Personal, Politics, Preferences, Psychology, Religion, Science, Society, Sports, Technology. Return one category only. Shortest responses only",
     // COC
     "You are a content moderator for a large social network. I will give you posts to rate. Respond with a rejection score from 0-100 only. Number only.",
     // FIRST NAME
@@ -36,5 +36,5 @@ module.exports = {
   QUEST_OPTION_ADDED_AMOUNT: 0.10,
   QUEST_OPTION_CONTENTION_GIVEN_AMOUNT: 0.10,
   QUEST_OPTION_CONTENTION_REMOVED_AMOUNT: 0.10,
-  MASTER_ARRAY_TOPICS: ["Architecture", "Arts", "Business", "Culture", "Economics", "Education", "Environment", "Ethics", "Geography", "Health", "History", "Law", "Pets", "Music", "Philosophy", "Personal", "Politics", "Preferences", "Psychology", "Religion", "Science", "Society", "Sports", "Technology"]
+  MASTER_ARRAY_TOPICS: ["Architecture", "Arts", "Business", "Culture", "Economics", "Education", "Entertainment", "Environment", "Ethics", "Food", "Geography", "Health", "History", "Hobbies", "Home", "Law", "Media", "Music", "Pets", "Philosophy", "Personal", "Politics", "Preferences", "Psychology", "Religion", "Science", "Society", "Sports", "Technology"]
 };
