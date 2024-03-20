add27e8709e449aaa9589df508a52ce00825287e
Wamiq <wamiqakram@gmail.com>
2024-03-20 18:10:07 +0500

    remove personal badge api integerated

--- routes/BadgesRoutes.js
+++ routes/BadgesRoutes.js
@@ -26,6 +26,8 @@ router.post("/removeBadge", BadgeController.removeBadge);
 
 router.post("/removeContactBadge", BadgeController.removeContactBadge);
 
+router.post("/removePersonalBadge", BadgeController.removePersonalBadge);
+
 router.post("/addBadge/web3/add", BadgeController.addWeb3Badge);
 
 
