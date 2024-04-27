3230451926dd31fdcfda367434a3a0e286f47899
mmahad911 <mmahad913@gmail.com>
2024-04-27 20:27:26 +0500

    Tab Opening Swagger Setting.
    Tab Opening Swagger Setting.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const app = require('./server');
 const connectDB = require("./config/db");
+const { exec } = require('child_process');
 
 dotenv.config();
 
@@ -11,4 +12,13 @@ let port = process.env.BASE_PORT;
 
 app.listen(port, () => {
   console.log("Server is listening on port: ", port);
+
+  // Open Swagger UI in default browser
+  exec(`start http://localhost:${port}/foundation-api-documentation-swagger`, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Failed to open Swagger UI in browser:', error);
+      return;
+    }
+    console.log('Swagger UI opened in default browser.');
+  });
 });
